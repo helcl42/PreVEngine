e01649b233e5a81833dedeefe738b882f80ee17f
helcll <lubos.helcl@gmail.com>
2019-11-22 02:58:00 +0100

    FIX: glm cant be aligned by default...

--- PreVEngine/Common.h
+++ PreVEngine/Common.h
@@ -3,7 +3,7 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+//#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
