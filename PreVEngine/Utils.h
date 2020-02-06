ae7d59482024901a007359cba2de0251e7901d65
helcll <lubos.helcl@gmail.com>
2020-02-07 00:11:12 +0100

    FIX: warning ->size_t vs. int

--- PreVEngine/Utils.h
+++ PreVEngine/Utils.h
@@ -369,7 +369,7 @@ namespace PreVEngine
 
 			std::random_device rd;
 			std::mt19937 gen(rd());
-			std::uniform_int_distribution<> dis(0, validSymbols.size() - 1);
+			std::uniform_int_distribution<> dis(0, static_cast<int>(validSymbols.size() - 1));
 			const auto index = dis(gen);
 			return validSymbols[index];
 		}
