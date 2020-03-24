bc1436e73b3808149fe00085e53dce0be1b15a05
helcll <lubos.helcl@gmail.com>
2020-03-24 20:53:04 +0100

    REF: minor

--- PreVEngineTest/Shadows.h
+++ PreVEngineTest/Shadows.h
@@ -25,10 +25,10 @@ struct ShadowsCascade {
     glm::mat4 GetBiasedViewProjectionMatrix() const
     {
         static const glm::mat4 biasMat(
-            0.5, 0.0, 0.0, 0.0,
-            0.0, 0.5, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            0.5, 0.5, 0.0, 1.0);
+            0.5f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.5f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            0.5f, 0.5f, 0.0f, 1.0f);
 
         return biasMat * projectionMatrix * viewMatrix;
     }
