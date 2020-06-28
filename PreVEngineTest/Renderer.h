9bf7118d340c35fa180cca4ba4a030100b1aa818
helcll <lubos.helcl@gmail.com>
2020-06-28 21:33:40 +0200

    FEAT: multi-mest -> multi-material

--- PreVEngineTest/Renderer.h
+++ PreVEngineTest/Renderer.h
@@ -1723,7 +1723,7 @@ public:
                     uboFS->Update(&uniformsFS);
 
                     m_shader->Bind("depthSampler", shadowsComponent->GetImageBuffer()->GetImageView(), shadowsComponent->GetImageBuffer()->GetSampler(), VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL);
-                    m_shader->Bind("textureSampler", *nodeRenderComponent->GetMaterial()->GetImageBuffer(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+                    m_shader->Bind("textureSampler", *nodeRenderComponent->GetMaterial(meshPart.materialIndex)->GetImageBuffer(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
                     m_shader->Bind("uboVS", *uboVS);
                     m_shader->Bind("uboFS", *uboFS);
 
