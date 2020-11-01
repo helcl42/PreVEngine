3da50f2514f8b81b1364febfb0cae923fb78afc4
helcll <lubos.helcl@gmail.com>
2020-11-01 12:48:28 +0100

    REF: custom config

--- PreVEngineTest/Main.cpp
+++ PreVEngineTest/Main.cpp
@@ -4,7 +4,17 @@ int main(int argc, char* argv[])
 {
     setvbuf(stdout, NULL, _IONBF, 0); // avoid buffering
 
+    auto sceneConfig = std::make_shared<prev::scene::SceneConfig>();
+    sceneConfig->framesInFlight = 3;
+    sceneConfig->VSync = true;
+
     auto config = std::make_shared<prev::core::EngineConfig>();
+    config->appName = "Test App";
+    config->fullScreen = false;
+    config->validation = true;
+    config->windowPosition = { 40, 40 };
+    config->windowSize = { 1920, 1080 };
+    config->sceneConfig = sceneConfig;
 
     prev_test::TestApp app(config);
     app.Init();
