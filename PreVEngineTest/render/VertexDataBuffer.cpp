feeba5f0bb48682c45601bb9fea0d14971dcb5b4
helcll <lubos.helcl@gmail.com>
2021-01-10 11:43:19 +0100

    REF: minor - generic vs. multiple overloads

--- PreVEngineTest/render/VertexDataBuffer.cpp
+++ PreVEngineTest/render/VertexDataBuffer.cpp
@@ -11,36 +11,6 @@ void VertexDataBuffer::Add(const void* data, const unsigned int size)
     m_buffer.insert(m_buffer.end(), static_cast<const uint8_t*>(data), static_cast<const uint8_t*>(data) + size);
 }
 
-void VertexDataBuffer::Add(const float data)
-{
-    Add(&data, sizeof(float));
-}
-
-void VertexDataBuffer::Add(const glm::vec2& data)
-{
-    Add(&data, sizeof(glm::vec2));
-}
-
-void VertexDataBuffer::Add(const glm::vec3& data)
-{
-    Add(&data, sizeof(glm::vec3));
-}
-
-void VertexDataBuffer::Add(const glm::vec4& data)
-{
-    Add(&data, sizeof(glm::vec4));
-}
-
-void VertexDataBuffer::Add(const glm::mat3& data)
-{
-    Add(&data, sizeof(glm::mat3));
-}
-
-void VertexDataBuffer::Add(const glm::mat4& data)
-{
-    Add(&data, sizeof(glm::mat4));
-}
-
 void VertexDataBuffer::Reset()
 {
     m_buffer.clear();
