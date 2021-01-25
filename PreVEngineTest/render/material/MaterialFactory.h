957e9cfd11e0e6a80b3251314d791774e771689b
helcll <lubos.helcl@gmail.com>
2021-01-25 01:01:57 +0100

    FEAT: material factory can create material from assimp model

--- PreVEngineTest/render/material/MaterialFactory.h
+++ PreVEngineTest/render/material/MaterialFactory.h
@@ -13,6 +13,8 @@ public:
     std::unique_ptr<prev_test::render::IMaterial> Create(const MaterialProperties& materialProps, const ImagePair& image, const ImagePair& normalMap) const;
 
     std::unique_ptr<prev_test::render::IMaterial> Create(const MaterialProperties& materialProps, const ImagePair& image, const ImagePair& normalMap, const ImagePair& heightMap) const;
+
+    std::vector<std::unique_ptr<prev_test::render::IMaterial> > Create(const std::string& modelPath) const;
 };
 } // namespace prev_test::render::material
 
