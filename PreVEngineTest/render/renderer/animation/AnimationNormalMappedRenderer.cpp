1ddd6177f68ea215018e08bde6fd620f9aa2942b
helcll <lubos.helcl@gmail.com>
2021-02-04 00:41:07 +0100

    FEAT: normal/height/conestep maps are not mandatory in renderers

--- PreVEngineTest/render/renderer/animation/AnimationNormalMappedRenderer.cpp
+++ PreVEngineTest/render/renderer/animation/AnimationNormalMappedRenderer.cpp
@@ -169,7 +169,9 @@ void AnimationNormalMappedRenderer::RenderMeshNode(const prev::render::RenderCon
         uboFS->Update(&uniformsFS);
 
         m_shader->Bind("textureSampler", *material->GetImageBuffer(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-        m_shader->Bind("normalSampler", *material->GetNormalmageBuffer(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+        if (material->GetNormalmageBuffer()) {
+            m_shader->Bind("normalSampler", *material->GetNormalmageBuffer(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+        }
         m_shader->Bind("depthSampler", shadowsComponent->GetImageBuffer()->GetImageView(), shadowsComponent->GetImageBuffer()->GetSampler(), VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL);
         m_shader->Bind("uboVS", *uboVS);
         m_shader->Bind("uboFS", *uboFS);
