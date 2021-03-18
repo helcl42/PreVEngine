cadf0b31ee419c9dc46e0c1d5737cbff533d9ab8
helcll <lubos.helcl@gmail.com>
2021-03-18 22:49:21 +0100

    FIX: image layout transitions.. again

--- PreVEngine/prev/core/memory/image/DepthImageBuffer.cpp
+++ PreVEngine/prev/core/memory/image/DepthImageBuffer.cpp
@@ -18,12 +18,6 @@ void DepthImageBuffer::Create(const ImageBufferCreateInfo& createInfo)
     m_sampler = VK_NULL_HANDLE;
 
     Resize(createInfo.extent);
-
-#if defined(__ANDROID__)
-    // nothing on android ??
-#else
-    m_allocator.TransitionImageLayout(m_image, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL, m_format, m_mipLevels, m_layerCount);
-#endif
 }
 
 void DepthImageBuffer::Resize(const VkExtent3D& extent)
@@ -31,6 +25,11 @@ void DepthImageBuffer::Resize(const VkExtent3D& extent)
     Destroy();
 
     m_allocator.CreateImage(extent, m_imageType, m_format, m_sampleCount, m_mipLevels, m_layerCount, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, m_flags, m_image, m_allocation);
+#if defined(__ANDROID__)
+    // nothing on android ??
+#else
+    m_allocator.TransitionImageLayout(m_image, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL, m_format, m_mipLevels, m_layerCount);
+#endif
     m_allocator.CreateImageView(m_image, m_format, m_imageViewType, m_mipLevels, m_layerCount, VK_IMAGE_ASPECT_DEPTH_BIT, m_imageView);
 
     m_extent = extent;
