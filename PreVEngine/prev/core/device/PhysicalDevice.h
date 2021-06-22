090661d05e5805846f304bb313e453a0af46cee7
helcll <lubos.helcl@gmail.com>
2021-06-22 21:52:48 +0200

    REF/FIX: user RGBA as default surface format

--- PreVEngine/prev/core/device/PhysicalDevice.h
+++ PreVEngine/prev/core/device/PhysicalDevice.h
@@ -24,15 +24,14 @@ public:
 
     std::vector<VkSurfaceFormatKHR> SurfaceFormats(VkSurfaceKHR surface) const; // Returns list of supported surface formats.
 
-    VkFormat FindSurfaceFormat(VkSurfaceKHR surface, const std::vector<VkFormat>& preferredFormats = { VK_FORMAT_B8G8R8A8_UNORM, // Returns first supported format from given list, or VK_FORMAT_UNDEFINED if no match was found.
-                                                         VK_FORMAT_R8G8B8A8_UNORM }) const;
+    VkFormat FindSurfaceFormat(VkSurfaceKHR surface, const std::vector<VkFormat>& preferredFormats = { VK_FORMAT_R8G8B8A8_UNORM, VK_FORMAT_B8G8R8A8_UNORM }) const; // Returns first supported format from given list, or VK_FORMAT_UNDEFINED if no match was found.
 
     VkFormat FindDepthFormat(const std::vector<VkFormat>& preferredFormats = {
-                                 VK_FORMAT_D32_SFLOAT, // Returns first supported depth format from list,
-                                 VK_FORMAT_D32_SFLOAT_S8_UINT, // or VK_FORMAT_UNDEFINED if no match was found.
+                                 VK_FORMAT_D32_SFLOAT,
+                                 VK_FORMAT_D32_SFLOAT_S8_UINT,
                                  VK_FORMAT_D24_UNORM_S8_UINT,
                                  VK_FORMAT_D16_UNORM_S8_UINT,
-                                 VK_FORMAT_D16_UNORM }) const;
+                                 VK_FORMAT_D16_UNORM }) const; // Returns first supported depth format from list, or VK_FORMAT_UNDEFINED if no match was found.
 
 public:
     const VkPhysicalDeviceProperties& GetProperties() const;
