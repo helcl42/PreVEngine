e194492d9c3d87cb673c767793d14d67014247bb
helcll <lubos.helcl@gmail.com>
2022-02-06 23:00:46 +0100

    REF: const shadows stuff - keep it in one place

--- PreVEngineTest/component/shadow/ShadowsCommon.h
+++ PreVEngineTest/component/shadow/ShadowsCommon.h
@@ -2,12 +2,17 @@
 #define __SHADOWS_COMMON_H__
 
 #include <prev/common/Common.h>
+#include <prev/core/Formats.h>
 
 namespace prev_test::component::shadow {
 
-constexpr bool SHADOWS_ENABLED{ true };
+constexpr const bool SHADOWS_ENABLED{ true };
 
-constexpr uint32_t CASCADES_COUNT{ 4 };
+constexpr const  uint32_t CASCADES_COUNT{ 4 };
+
+constexpr const VkFormat DEPTH_FORMAT{ VK_FORMAT_D32_SFLOAT };
+
+constexpr const uint32_t SHADOW_MAP_DIMENSIONS{ 2048 };
 
 } // namespace prev_test::component::shadow
 
--- PreVEngineTest/component/shadow/ShadowsComponentFactory.h
+++ PreVEngineTest/component/shadow/ShadowsComponentFactory.h
@@ -18,11 +18,6 @@ private:
     std::unique_ptr<prev::core::memory::image::IImageBuffer> CreateDepthBuffer(const VkExtent2D& extent, const uint32_t cascadesCount, prev::core::memory::Allocator& allocator) const;
 
     std::vector<ShadowsCascade> CreateCascades(const VkExtent2D& extent, const uint32_t cascadesCount, const std::shared_ptr<prev::core::memory::image::IImageBuffer>& depthBuffer, const std::shared_ptr<prev::render::pass::RenderPass>& renderPass, prev::core::device::Device& device) const;
-
-private:
-    static const inline VkFormat DEPTH_FORMAT{ VK_FORMAT_D32_SFLOAT };
-
-    static const inline uint32_t SHADOW_MAP_DIMENSIONS{ 2048 };
 };
 } // namespace prev_test::component::shadow
 
