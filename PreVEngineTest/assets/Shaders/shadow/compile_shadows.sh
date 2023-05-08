a35b4225b4b991016816be343b031f288a1069d4
helcll <lubos.helcl@gmail.com>
2023-05-08 22:16:15 +0200

    REF: minor -> lights

--- PreVEngineTest/assets/Shaders/shadow/compile_shadows.sh
+++ PreVEngineTest/assets/Shaders/shadow/compile_shadows.sh
@@ -22,6 +22,6 @@ mv vert.spv animation_bump_mapped_shadows_vert.spv
 
 glslangValidator -V terrain_bump_mapped_shadows.vert
 
-move /y vert.spv terrain_bump_mapped_shadows_vert.spv
+mv /y vert.spv terrain_bump_mapped_shadows_vert.spv
 
 exit
