cdf0339cfe84b32b5f9838519b1f09fb339a3cc7
helcll <lubos.helcl@gmail.com>
2023-05-23 21:49:41 +0200

    FIX: disable GLM experimental

--- PreVEngine/prev/common/Common.h
+++ PreVEngine/prev/common/Common.h
@@ -4,7 +4,7 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 // #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
-#define GLM_ENABLE_EXPERIMENTAL
+// #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/hash.hpp>
