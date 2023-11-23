0597d65bc88473c442327e1dddcd5ac8b50de374
helcll <lubos.helcl@gmail.com>
2023-11-23 01:30:01 +0100

    FIX/OPT: added bounding volumes to particles

--- PreVEngineTest/scene/Fire.h
+++ PreVEngineTest/scene/Fire.h
@@ -3,6 +3,7 @@
 
 #include "../General.h"
 #include "../component/particle/IParticleSystemComponent.h"
+#include "../component/ray_casting/IBoundingVolumeComponent.h"
 
 #include <prev/scene/graph/SceneNode.h>
 
@@ -26,6 +27,8 @@ private:
     const glm::vec3 m_initialPosition;
 
     std::shared_ptr<prev_test::component::particle::IParticleSystemComponent> m_particleSystemComponent;
+
+    std::shared_ptr<prev_test::component::ray_casting::IBoundingVolumeComponent> m_boundingVolumeComponent;
 };
 } // namespace prev_test::scene
 
