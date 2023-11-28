dd918d3ac429038a88d454faf9cdf8c53e2a1c21
helcll <lubos.helcl@gmail.com>
2023-11-28 20:32:16 +0100

    FIX/OPT: nonsense one-to-one commandPool-commandBuffer relation in CommandBuffersGroup

--- PreVEngineTest/render/renderer/CommandBuffersGroupFactory.cpp
+++ PreVEngineTest/render/renderer/CommandBuffersGroupFactory.cpp
@@ -5,23 +5,17 @@
 namespace prev_test::render::renderer {
 std::unique_ptr<CommandBuffersGroup> CommandBuffersGroupFactory::CreateGroup(const prev::core::device::Device& device, const prev::core::device::Queue& queue, const uint32_t dim0Size, const uint32_t groupSize, const VkCommandBufferLevel level) const
 {
-    std::vector<std::vector<VkCommandPool>> commandPools{};
-    std::vector<std::vector<VkCommandBuffer>> commandBuffers{};
-    commandPools.reserve(dim0Size);
+    VkCommandPool commandPool = prev::util::vk::CreateCommandPool(device, queue.family);
+    std::vector<std::vector<VkCommandBuffer>> commandBuffers;
     commandBuffers.reserve(dim0Size);
-
-    for (uint32_t dim0Index = 0; dim0Index < dim0Size; dim0Index++) {
-        std::vector<VkCommandPool> pools{};
-        std::vector<VkCommandBuffer> buffers{};
-        for (uint32_t shadowRendererIndex = 0; shadowRendererIndex < groupSize; shadowRendererIndex++) {
-            auto pool = queue.CreateCommandPool();
-            pools.push_back(pool);
-            buffers.push_back(prev::util::vk::CreateCommandBuffer(device, pool, level));
+    for (uint32_t dim0Index = 0; dim0Index < dim0Size; ++dim0Index) {
+        std::vector<VkCommandBuffer> buffers(groupSize);
+        for (uint32_t index = 0; index < groupSize; ++index) {
+            buffers[index] = prev::util::vk::CreateCommandBuffer(device, commandPool, level);
         }
-        commandPools.push_back(pools);
-        commandBuffers.push_back(buffers);
+        commandBuffers.emplace_back(buffers);
     }
 
-    return std::make_unique<CommandBuffersGroup>(device, commandPools, commandBuffers);
+    return std::make_unique<CommandBuffersGroup>(device, commandPool, commandBuffers);
 }
 } // namespace prev_test::render::renderer
