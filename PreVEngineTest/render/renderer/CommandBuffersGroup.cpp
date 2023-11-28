d6bdc38fa2e0cbbeacbc8adda3a0e6fbba572406
helcll <lubos.helcl@gmail.com>
2023-11-28 20:32:16 +0100

    FIX/OPT: nonsense one-to-one commandPool-commandBuffer relation in CommandBuffersGroup

--- PreVEngineTest/render/renderer/CommandBuffersGroup.cpp
+++ PreVEngineTest/render/renderer/CommandBuffersGroup.cpp
@@ -1,9 +1,9 @@
 #include "CommandBuffersGroup.h"
 
 namespace prev_test::render::renderer {
-CommandBuffersGroup::CommandBuffersGroup(const prev::core::device::Device& device, const std::vector<std::vector<VkCommandPool>>& pools, const std::vector<std::vector<VkCommandBuffer>>& commandBuffers)
+CommandBuffersGroup::CommandBuffersGroup(const prev::core::device::Device& device, const VkCommandPool& commandPool, const std::vector<std::vector<VkCommandBuffer>>& commandBuffers)
     : m_device(device)
-    , m_commandPoolGroups(pools)
+    , m_commandPool(commandPool)
     , m_commandBufferGroups(commandBuffers)
 {
 }
@@ -12,11 +12,7 @@ CommandBuffersGroup::~CommandBuffersGroup()
 {
     vkDeviceWaitIdle(m_device);
 
-    for (auto& poolGroup : m_commandPoolGroups) {
-        for (auto& pool : poolGroup) {
-            vkDestroyCommandPool(m_device, pool, nullptr);
-        }
-    }
+    vkDestroyCommandPool(m_device, m_commandPool, nullptr);
 }
 
 const std::vector<VkCommandBuffer>& CommandBuffersGroup::GetBuffersGroup(const uint32_t index) const
