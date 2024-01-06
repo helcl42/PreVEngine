b750c3fd0e763c8d3cb02e1daecc35e70fe96fcb
helcll <lubos.helcl@gmail.com>
2024-01-06 16:31:08 +0100

    FIX: normal renormalization after normal map sampling

--- PreVEngineTest/assets/Shaders/common/normal_mapping_use.glsl
+++ PreVEngineTest/assets/Shaders/common/normal_mapping_use.glsl
@@ -2,5 +2,5 @@
 
 vec3 NormalMapping(in sampler2D normalMapSampler, in vec2 uv)
 {
-    return normalize(2.0 * texture(normalMapSampler, uv).xyz - 1.0);
+    return 2.0 * normalize(texture(normalMapSampler, uv).xyz) - 1.0;
 }
