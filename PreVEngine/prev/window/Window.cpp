fe4e1a13d34b237c4fbea3f87291ce77f32b0c14
helcll <lubos.helcl@gmail.com>
2024-03-20 12:21:49 +0100

    REF: simplified and cleaned up window

--- PreVEngine/prev/window/Window.cpp
+++ PreVEngine/prev/window/Window.cpp
@@ -1,66 +1,120 @@
 #include "Window.h"
+#include "InputConvertor.h"
+#include "impl/WindowImplFactory.h"
 
-#include "../event/EventChannel.h"
+#include <stdexcept>
 
 namespace prev::window {
 Window::Window(const WindowCreateInfo& createInfo)
-    : AbstractWindow(createInfo)
 {
+    m_windowImpl = impl::WindowImplFactory{}.Create(impl::WindowInfo{ createInfo.title, { createInfo.left, createInfo.top }, { createInfo.width, createInfo.height }, createInfo.fullScreen });
 }
 
-void Window::OnInitEvent()
+impl::Surface& Window::GetSurface(VkInstance instance)
 {
-    prev::event::EventChannel::Post(WindowCreatedEvent{ this });
+    if (!m_windowImpl->CreateSurface(instance)) {
+        throw std::runtime_error("Could not create surface.");
+    }
+    return *m_windowImpl;
 }
 
-void Window::OnCloseEvent()
+bool Window::CanPresent(VkPhysicalDevice gpu, uint32_t queueFamily) const
 {
-    prev::event::EventChannel::Post(WindowDestroyedEvent{ this });
+    return m_windowImpl->CanPresent(gpu, queueFamily);
 }
 
-void Window::OnChangeEvent()
+impl::Position Window::GetPosition() const
 {
-    prev::event::EventChannel::Post(WindowChangeEvent{ this });
+    const auto& shape{ m_windowImpl->GetInfo() };
+    return shape.position;
 }
 
-void Window::OnResizeEvent(uint32_t width, uint32_t height)
+impl::Size Window::GetSize() const
 {
-    prev::event::EventChannel::Post(WindowResizeEvent{ this, width, height });
+    const auto& shape{ m_windowImpl->GetInfo() };
+    return shape.size;
 }
 
-void Window::OnMoveEvent(int32_t x, int32_t y)
+bool Window::IsKeyPressed(const prev::input::keyboard::KeyCode key) const
 {
-    prev::event::EventChannel::Post(WindowMovedEvent{ this, glm::vec2(x, y) });
+    return m_windowImpl->IsKeyPressed(key);
 }
 
-void Window::OnFocusEvent(bool hasFocus)
+bool Window::IsMouseButtonPressed(const impl::ButtonType btn) const
 {
-    prev::event::EventChannel::Post(WindowFocusChangeEvent{ this, hasFocus });
+    return m_windowImpl->IsMouseButtonPressed(btn);
 }
 
-void Window::OnKeyEvent(impl::ActionType action, prev::input::keyboard::KeyCode keyCode)
+impl::Position Window::GetMousePosition() const
 {
-    prev::event::EventChannel::Post(prev::input::keyboard::KeyEvent{ InputConvertor::GetKeyActionType(action), keyCode });
+    return m_windowImpl->GetMousePosition();
 }
 
-void Window::OnMouseEvent(impl::ActionType action, int32_t x, int32_t y, impl::ButtonType button, uint32_t w, uint32_t h)
+bool Window::HasFocus() const
 {
-    prev::event::EventChannel::Post(prev::input::mouse::MouseEvent{ InputConvertor::GetMouseActionType(action), InputConvertor::GetMouseButtonType(button), glm::vec2(x, y), glm::vec2(w, h) });
+    return m_windowImpl->HasFocus();
 }
 
-void Window::OnMouseScrollEvent(int32_t delta, int32_t x, int32_t y)
+bool Window::IsMouseLocked() const
 {
-    prev::event::EventChannel::Post(prev::input::mouse::MouseScrollEvent{ delta, glm::vec2(x, y) });
+    return m_windowImpl->IsMouseLocked();
 }
 
-void Window::OnTouchEvent(impl::ActionType action, float x, float y, uint8_t pointerId, float w, float h)
+bool Window::IsMouseCursorVisible() const
 {
-    prev::event::EventChannel::Post(prev::input::touch::TouchEvent{ InputConvertor::GetTouchActionType(action), pointerId, glm::vec2(x, y), glm::vec2(w, h) });
+    return m_windowImpl->IsMouseCursorVisible();
 }
 
-void Window::OnTextEvent(const char* str)
+void Window::SetTitle(const std::string& title)
 {
-    prev::event::EventChannel::Post(prev::input::keyboard::TextEvent{ str });
+    m_windowImpl->SetTitle(title);
+}
+
+void Window::SetPosition(const impl::Position& position)
+{
+    m_windowImpl->SetPosition(position.x, position.y);
+}
+
+void Window::SetSize(const impl::Size& size)
+{
+    m_windowImpl->SetSize(size.width, size.height);
+}
+
+void Window::ShowKeyboard(bool enabled) // On Android, show the soft-keyboard.
+{
+    m_windowImpl->SetTextInput(enabled);
+}
+
+void Window::SetMouseLocked(bool locked)
+{
+    m_windowImpl->SetMouseLocked(locked);
+}
+
+void Window::SetMouseCursorVisible(bool visible)
+{
+    m_windowImpl->SetMouseCursorVisible(visible);
+}
+
+void Window::Close()
+{
+    m_windowImpl->Close();
+}
+
+impl::Event Window::GetEvent(bool waitForEvent)
+{
+    return m_windowImpl->GetEvent(waitForEvent);
+}
+
+bool Window::ProcessEvents(bool waitForEvent)
+{
+    impl::Event e = m_windowImpl->GetEvent(waitForEvent);
+    while (e.tag != impl::Event::EventType::NONE) {
+        if (!ProcessEvent(e)) {
+            return false;
+        }
+        e = m_windowImpl->GetEvent();
+    }
+    return m_windowImpl->IsRunning();
 }
 
 void Window::operator()(const prev::input::mouse::MouseLockRequest& mouseLock)
@@ -73,4 +127,45 @@ void Window::operator()(const prev::input::mouse::MouseCursorVisibilityRequest& 
     SetMouseCursorVisible(cursorVisibility.visible);
 }
 
+bool Window::ProcessEvent(const impl::Event& e)
+{
+    switch (e.tag) {
+    case impl::Event::EventType::MOUSE:
+        prev::event::EventChannel::Post(prev::input::mouse::MouseEvent{ InputConvertor::GetMouseActionType(e.mouse.action), InputConvertor::GetMouseButtonType(e.mouse.btn), glm::vec2(e.mouse.x, e.mouse.y), glm::vec2(e.mouse.w, e.mouse.h) });
+        break;
+    case impl::Event::EventType::MOUSE_SCROLL:
+        prev::event::EventChannel::Post(prev::input::mouse::MouseScrollEvent{ e.scroll.delta, glm::vec2(e.scroll.x, e.scroll.y) });
+        break;
+    case impl::Event::EventType::KEY:
+        prev::event::EventChannel::Post(prev::input::keyboard::KeyEvent{ InputConvertor::GetKeyActionType(e.key.action), e.key.keyCode });
+        break;
+    case impl::Event::EventType::TEXT:
+        prev::event::EventChannel::Post(prev::input::keyboard::TextEvent{ e.text.str });
+        break;
+    case impl::Event::EventType::MOVE:
+        prev::event::EventChannel::Post(WindowMovedEvent{ this, glm::vec2(e.move.x, e.move.y) });
+        break;
+    case impl::Event::EventType::RESIZE:
+        prev::event::EventChannel::Post(WindowResizeEvent{ this, e.resize.width, e.resize.height });
+        break;
+    case impl::Event::EventType::FOCUS:
+        prev::event::EventChannel::Post(WindowFocusChangeEvent{ this, e.focus.hasFocus });
+        break;
+    case impl::Event::EventType::TOUCH:
+        prev::event::EventChannel::Post(prev::input::touch::TouchEvent{ InputConvertor::GetTouchActionType(e.touch.action), e.touch.id, glm::vec2(e.touch.x, e.touch.y), glm::vec2(e.touch.w, e.touch.h) });
+        break;
+    case impl::Event::EventType::INIT:
+        prev::event::EventChannel::Post(WindowCreatedEvent{ this });
+        break;
+    case impl::Event::EventType::CHANGE:
+        prev::event::EventChannel::Post(WindowChangeEvent{ this });
+        break;
+    case impl::Event::EventType::CLOSE:
+        prev::event::EventChannel::Post(WindowDestroyedEvent{ this });
+        return false;
+    default:
+        break;
+    }
+    return true;
+}
 } // namespace prev::window
