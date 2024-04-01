0842cd47fc92448151d65dabcd931e800dfc9b11
Lubos Helcl <lubos.helcl@gmail.com>
2024-04-01 23:24:27 +0200

    FEAT: iOS - added single point touch event handling

--- PreVEngine/platform/ios/IOSView.mm
+++ PreVEngine/platform/ios/IOSView.mm
@@ -2,4 +2,52 @@
 
  @implementation IOSView
 
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if(self) {
+        self->touchState = NONE;
+        self->point = CGPoint();
+    }
+    return self;
+}
+
+- (void)resetTouchState
+{
+    self->touchState = NONE;
+    self->point = CGPoint();
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch* firstTouch = [touches anyObject];
+    CGPoint touchLocation = [firstTouch locationInView:self];
+    
+    self->touchState = DOWN;
+    self->point = touchLocation;
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch* firstTouch = [touches anyObject];
+    CGPoint touchLocation = [firstTouch locationInView:self];
+    
+    self->touchState = MOVE;
+    self->point = touchLocation;
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch* firstTouch = [touches anyObject];
+    CGPoint touchLocation = [firstTouch locationInView:self];
+    
+    self->touchState = UP;
+    self->point = touchLocation;
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self resetTouchState];
+}
+
  @end
