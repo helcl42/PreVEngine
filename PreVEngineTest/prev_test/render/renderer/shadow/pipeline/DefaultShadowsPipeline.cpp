c0031cf394e300f3e9b119bc8d724bc77344e5f5
helcll <lubos.helcl@gmail.com>
2024-07-05 00:41:04 +0200

    FIX/REF: pipeline creation - Pt. II

--- PreVEngineTest/prev_test/render/renderer/shadow/pipeline/DefaultShadowsPipeline.cpp
+++ PreVEngineTest/prev_test/render/renderer/shadow/pipeline/DefaultShadowsPipeline.cpp
@@ -12,7 +12,7 @@ bool DefaultShadowsPipeline::Init()
 {
     prev_test::render::pipeline::PipelineFactory pipelineFactory{};
     m_pipelineLayout = pipelineFactory.CreatePipelineLayout(m_device, m_shaders);
-    m_pipeline = pipelineFactory.CreateShadowsPipeline(m_device, m_shaders, m_renderPass, m_pipelineLayout);
+    m_pipeline = pipelineFactory.CreateDefaultPipeline(m_device, m_shaders, m_renderPass, m_pipelineLayout, VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, true, true, false, false, true);
     return m_pipeline != VK_NULL_HANDLE;
 }
 } // namespace prev_test::render::renderer::shadow::pipeline
