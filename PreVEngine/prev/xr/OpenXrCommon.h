3d423fb57bda1af7018a530d45fd70e363bfee60
helcll <lubos.helcl@gmail.com>
2024-12-16 02:14:23 +0100

    Fix XR platform resolution

--- PreVEngine/prev/xr/OpenXrCommon.h
+++ PreVEngine/prev/xr/OpenXrCommon.h
@@ -6,8 +6,15 @@
 #include "../core/Core.h"
 #include "../common/Logger.h"
 
-// TODO - switch platforms here
+#if defined(TARGET_PLATFORM_ANDROID)
 #define XR_USE_PLATFORM_ANDROID
+#elif defined(TARGET_PLATFORM_WINDOWS)
+#define XR_USE_PLATFORM_WIN32
+#elif defined(TARGET_PLATFORM_LINUX)
+#define XR_USE_PLATFORM_XCB
+#else
+#error Unsupported XR platform.
+#endif
 
 #define XR_USE_GRAPHICS_API_VULKAN
 #include <openxr/openxr.h>
