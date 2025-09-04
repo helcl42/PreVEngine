cc4f107a0c312b5819340f1b06f2f31153ca04cb
helcll <lubos.helcl@gmail.com>
2025-09-04 23:43:50 +0200

    Refactor animation

--- PreVEngineTest/prev_test/render/animation/Animation.h
+++ PreVEngineTest/prev_test/render/animation/Animation.h
@@ -3,84 +3,14 @@
 
 #include "../IAnimation.h"
 
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
+#include "AnimationClip.h"
 
-#include <map>
 #include <memory>
 
 namespace prev_test::render::animation {
-class AnimationFactory;
-
-// TODO -> make AnimationClipImpl* member and hide there all the Assimp specific things
-class AnimationClip : public prev_test::render::IAnimationClip {
-public:
-    AnimationClip(const aiScene* scene);
-
-    ~AnimationClip() = default;
-
-public:
-    void Update(const float deltaTime) override;
-
-    const std::vector<glm::mat4>& GetBoneTransforms() const override;
-
-    void SetState(const prev_test::render::AnimationState state) override;
-
-    void SetSpeed(const float speed) override;
-
-    void SetTime(const float elapsed) override;
-
-private:
-    uint32_t FindPosition(const float animationTime, const aiNodeAnim* nodeAnimation) const;
-
-    uint32_t FindRotation(const float animationTime, const aiNodeAnim* nodeAnimation) const;
-
-    uint32_t FindScaling(const float animationTime, const aiNodeAnim* nodeAnimation) const;
-
-    const aiNodeAnim* FindNodeAnimByName(const aiAnimation* animation, const std::string& nodeName) const;
-
-    aiVector3D CalculateInterpolatedPosition(const float animationTime, const aiNodeAnim* nodeAnimation) const;
-
-    aiQuaternion CalculateInterpolatedRotation(const float animationTime, const aiNodeAnim* nodeAnimation) const;
-
-    aiVector3D CalculateInterpolatedScaling(const float animationTime, const aiNodeAnim* nodeAnimation) const;
-
-    void UpdateNodeHeirarchy(const float animationTime, const aiNode* node, const glm::mat4& parentTransformation);
-
-private:
-    friend AnimationFactory;
-
-private:
-    struct BoneInfo {
-        glm::mat4 boneOffset;
-
-        glm::mat4 finalTransformation;
-    };
-
-private:
-    const aiScene* m_scene{};
-
-    float m_elapsedTime{ 0.0f };
-
-    std::map<std::string, uint32_t> m_boneMapping; // maps a bone name to its index
-
-    std::vector<BoneInfo> m_boneInfos;
-
-    glm::mat4 m_globalInverseTransform{ 1.0f };
-
-    std::vector<glm::mat4> m_boneTransforms;
-
-    prev_test::render::AnimationState m_animationState{ prev_test::render::AnimationState::RUNNING };
-
-    uint32_t m_index{ 0 };
-
-    float m_animationSpeed{ 1.0f };
-};
-
-// TODO create AnimationImpl member and hide there all the Assimp specific things ???
 class Animation : public prev_test::render::IAnimation {
 public:
-    Animation() = default;
+    Animation(std::vector<std::unique_ptr<IAnimationClip>>&& clips);
 
     ~Animation() = default;
 
@@ -98,14 +28,7 @@ public:
     void SetTime(const float elapsed) override;
 
 private:
-    friend AnimationFactory;
-
-private:
-    const aiScene* m_scene{};
-
-    Assimp::Importer m_importer; // TODO get rid of that
-
-    std::vector<std::shared_ptr<IAnimationClip>> m_clips;
+    std::vector<std::unique_ptr<IAnimationClip>> m_clips;
 };
 
 } // namespace prev_test::render::animation
--- PreVEngineTest/prev_test/render/animation/AnimationFactory.cpp
+++ PreVEngineTest/prev_test/render/animation/AnimationFactory.cpp
@@ -4,41 +4,111 @@
 #include "../util/assimp/AssimpGlmConvertor.h"
 #include "../util/assimp/AssimpSceneLoader.h"
 
+#include <prev/common/Logger.h>
+
 #include <stdexcept>
 
 namespace prev_test::render::animation {
-std::unique_ptr<prev_test::render::IAnimation> AnimationFactory::Create(const std::string& modelPath, const uint32_t clipIndex) const
+namespace {
+    VectorKey CreateVectorKey(const aiVectorKey& aiVectorKey)
+    {
+        return { prev_test::render::util::assimp::AssimpGlmConvertor::ToGlmVec3(aiVectorKey.mValue), static_cast<float>(aiVectorKey.mTime) };
+    }
+
+    QuaternionKey CreateQuaternionKey(const aiQuatKey& aiQuaternionKey)
+    {
+        return { prev_test::render::util::assimp::AssimpGlmConvertor::ToGlmQuat(aiQuaternionKey.mValue), static_cast<float>(aiQuaternionKey.mTime) };
+    }
+
+    AnimationNodeKeyFrames ConvertToNodeToAnimationNode(const aiNodeAnim& aiNode)
+    {
+        AnimationNodeKeyFrames keyFrames{};
+        keyFrames.name = std::string(aiNode.mNodeName.data);
+        keyFrames.positions.resize(aiNode.mNumPositionKeys);
+        for (uint32_t i = 0; i < aiNode.mNumPositionKeys; ++i) {
+            keyFrames.positions[i] = CreateVectorKey(aiNode.mPositionKeys[i]);
+        }
+        keyFrames.rotations.resize(aiNode.mNumRotationKeys);
+        for (uint32_t i = 0; i < aiNode.mNumRotationKeys; ++i) {
+            keyFrames.rotations[i] = CreateQuaternionKey(aiNode.mRotationKeys[i]);
+        }
+        keyFrames.scales.resize(aiNode.mNumScalingKeys);
+        for (uint32_t i = 0; i < aiNode.mNumScalingKeys; ++i) {
+            keyFrames.scales[i] = CreateVectorKey(aiNode.mScalingKeys[i]);
+        }
+        return keyFrames;
+    }
+
+    void CreateNodeHierarchy(const aiNode& aiNode, AnimationNode& outNode)
+    {
+        outNode.name = std::string(aiNode.mName.data);
+        outNode.transform = prev_test::render::util::assimp::AssimpGlmConvertor::ToGlmMat4(aiNode.mTransformation);
+        outNode.children.resize(aiNode.mNumChildren);
+        for (uint32_t i = 0; i < aiNode.mNumChildren; ++i) {
+            CreateNodeHierarchy(*aiNode.mChildren[i], outNode.children[i]);
+        }
+    }
+
+    std::unique_ptr<AnimationClip> CreateAnimationClip(const aiScene& scene, const aiAnimation& animation, const std::vector<BoneInfo>& bones)
+    {
+        const auto globalInverseTransform{ prev_test::render::util::assimp::AssimpGlmConvertor::ToGlmMat4(scene.mRootNode->mTransformation) };
+
+        AnimationNode rootNode{};
+        CreateNodeHierarchy(*scene.mRootNode, rootNode);
+
+        std::vector<AnimationNodeKeyFrames> keyFrames;
+        keyFrames.resize(animation.mNumChannels);
+        for (uint32_t nodeIndex = 0; nodeIndex < animation.mNumChannels; ++nodeIndex) {
+            const auto& animationNode{ *animation.mChannels[nodeIndex] };
+            keyFrames[nodeIndex] = ConvertToNodeToAnimationNode(animationNode);
+        }
+
+        if (animation.mDuration == 0) {
+            LOGW("Animation clip has duration of 0!");
+        }
+
+        const auto ticksPerSecond{ animation.mTicksPerSecond != 0 ? static_cast<float>(animation.mTicksPerSecond) : 25.0f };
+        const auto duration{ static_cast<float>(animation.mDuration) };
+
+        return std::make_unique<AnimationClip>(globalInverseTransform, rootNode, keyFrames, bones, ticksPerSecond, duration);
+    }
+
+    std::vector<std::unique_ptr<AnimationClip>> CreateAnimationClips(const aiScene& scene, const aiMesh& mesh)
+    {
+        std::vector<BoneInfo> bones;
+        bones.resize(mesh.mNumBones);
+        for (uint32_t i = 0; i < mesh.mNumBones; ++i) {
+            const auto& bone{ *mesh.mBones[i] };
+            bones[i] = BoneInfo{ bone.mName.data, prev_test::render::util::assimp::AssimpGlmConvertor::ToGlmMat4(bone.mOffsetMatrix) };
+        }
+
+        std::vector<std::unique_ptr<AnimationClip>> clips;
+        for (uint32_t animationIndex = 0; animationIndex < scene.mNumAnimations; ++animationIndex) {
+            const auto& animation{ *scene.mAnimations[animationIndex] };
+            clips.emplace_back(CreateAnimationClip(scene, animation, bones));
+        }
+        return clips;
+    }
+} // namespace
+
+std::unique_ptr<prev_test::render::IAnimation> AnimationFactory::Create(const std::string& modelPath) const
 {
-    // TODO copy asiimp animations to some local structure to avoid storing importer and whole scene !!!
-    auto animation = std::make_unique<Animation>();
+    const aiScene* scene{};
+    Assimp::Importer importer{};
 
     prev_test::render::util::assimp::AssimpSceneLoader assimpSceneLoader{};
-    if (!assimpSceneLoader.LoadScene(modelPath, animation->m_importer, &animation->m_scene)) {
+    if (!assimpSceneLoader.LoadScene(modelPath, importer, &scene)) {
         throw std::runtime_error("Could not load model: " + modelPath);
     }
 
-    for (uint32_t meshIndex = 0; meshIndex < animation->m_scene->mNumMeshes; ++meshIndex) {
-        const auto mesh = animation->m_scene->mMeshes[meshIndex];
-
-        auto animationClip = std::make_shared<AnimationClip>(animation->m_scene);
-        animationClip->m_globalInverseTransform = glm::inverse(prev_test::render::util::assimp::AssimpGlmConvertor::ToGlmMat4(animation->m_scene->mRootNode->mTransformation));
-        animationClip->m_index = clipIndex;
-
-        for (uint32_t i = 0; i < mesh->mNumBones; ++i) {
-            const auto bone = mesh->mBones[i];
-            const std::string boneName{ bone->mName.data };
-            if (animationClip->m_boneMapping.find(boneName) == animationClip->m_boneMapping.cend()) {
-                animationClip->m_boneMapping[boneName] = static_cast<uint32_t>(animationClip->m_boneInfos.size());
-
-                AnimationClip::BoneInfo boneInfo{};
-                boneInfo.boneOffset = prev_test::render::util::assimp::AssimpGlmConvertor::ToGlmMat4(bone->mOffsetMatrix);
-                animationClip->m_boneInfos.push_back(boneInfo);
-            }
+    std::vector<std::unique_ptr<IAnimationClip>> clips{};
+    for (uint32_t meshIndex = 0; meshIndex < scene->mNumMeshes; ++meshIndex) {
+        const auto& mesh{ *scene->mMeshes[meshIndex] };
+        auto meshClips{ CreateAnimationClips(*scene, mesh) };
+        for (auto& meshClip : meshClips) {
+            clips.emplace_back(std::move(meshClip));
         }
-
-        animation->m_clips.push_back(animationClip);
     }
-
-    return animation;
+    return std::make_unique<Animation>(std::move(clips));
 }
 } // namespace prev_test::render::animation
