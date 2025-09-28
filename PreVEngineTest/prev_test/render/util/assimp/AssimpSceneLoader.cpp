62e0d0968f68dc64f0e3a64f6effc3b229e5d5da
helcll <lubos.helcl@gmail.com>
2025-09-28 03:34:15 +0200

    Assimp: pass path as hint arg

--- PreVEngineTest/prev_test/render/util/assimp/AssimpSceneLoader.cpp
+++ PreVEngineTest/prev_test/render/util/assimp/AssimpSceneLoader.cpp
@@ -8,7 +8,7 @@ bool AssimpSceneLoader::LoadScene(const std::string& modelPath, Assimp::Importer
     const unsigned int flags = aiProcess_CalcTangentSpace | aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_SortByPType | aiProcess_GenSmoothNormals | aiProcess_FixInfacingNormals | aiProcess_FindInvalidData | aiProcess_ForceGenNormals | aiProcess_ValidateDataStructure;
 
     const std::vector<char> fileData{ prev::util::file::ReadBinaryFile(modelPath) };
-    *scene = importer.ReadFileFromMemory(fileData.data(), fileData.size(), flags);
+    *scene = importer.ReadFileFromMemory(fileData.data(), fileData.size(), flags, modelPath.c_str());
     if (!*scene) {
         return false;
     }
