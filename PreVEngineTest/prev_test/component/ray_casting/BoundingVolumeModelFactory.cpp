660fee633e4ba883ddf5bf0729795d45c9acabc8
helcll <lubos.helcl@gmail.com>
2025-10-01 01:52:00 +0200

    Fix bounding volume models

--- PreVEngineTest/prev_test/component/ray_casting/BoundingVolumeModelFactory.cpp
+++ PreVEngineTest/prev_test/component/ray_casting/BoundingVolumeModelFactory.cpp
@@ -6,7 +6,64 @@
 
 namespace prev_test::component::ray_casting {
 namespace {
-    std::tuple<std::vector<glm::vec3>, std::vector<uint32_t>> GenerateBoxData(const std::vector<glm::vec3>& boxPoints)
+    class BoundingVolumeMesh : public prev_test::render::IMesh {
+    public:
+        BoundingVolumeMesh(const std::vector<glm::vec3>& vertices, const std::vector<uint32_t>& indices)
+            : m_vertices{ vertices }
+            , m_indices{ indices }
+        {
+            m_meshParts.push_back(prev_test::render::MeshPart(static_cast<uint32_t>(indices.size()), vertices));
+        }
+
+    public:
+        const prev_test::render::VertexLayout& GetVertexLayout() const override
+        {
+            return vertexLayout;
+        }
+
+        const void* GetVertexData() const override
+        {
+            return m_vertices.data();
+        }
+
+        uint32_t GerVerticesCount() const override
+        {
+            return static_cast<uint32_t>(m_vertices.size());
+        }
+
+        const std::vector<uint32_t>& GetIndices() const override
+        {
+            return m_indices;
+        }
+
+        uint32_t GetIndicesCount() const override
+        {
+            return static_cast<uint32_t>(m_indices.size());
+        }
+
+        const std::vector<prev_test::render::MeshPart>& GetMeshParts() const override
+        {
+            return m_meshParts;
+        }
+
+        const prev_test::render::MeshNode& GetRootNode() const override
+        {
+            return meshRootNode;
+        }
+
+    private:
+        std::vector<glm::vec3> m_vertices;
+
+        std::vector<uint32_t> m_indices;
+
+        std::vector<prev_test::render::MeshPart> m_meshParts;
+
+        static const inline prev_test::render::VertexLayout vertexLayout{ { prev_test::render::VertexLayoutComponent::VEC3 } };
+
+        static const inline prev_test::render::MeshNode meshRootNode{ { 0 }, glm::mat4(1.0f), {} };
+    };
+
+    std::shared_ptr<prev_test::render::IMesh> GenerateBoxMesh(const std::vector<glm::vec3>& boxPoints)
     {
         const std::vector<glm::vec3> vertices = {
             // front
@@ -50,10 +107,10 @@ namespace {
             20, 21, 22, 22, 23, 20
         };
 
-        return { vertices, indices };
+        return std::make_shared<BoundingVolumeMesh>(vertices, indices);
     }
 
-    std::tuple<std::vector<glm::vec3>, std::vector<uint32_t>> GenerateSphereData(const prev_test::common::intersection::Sphere& sphere)
+    std::shared_ptr<prev_test::render::IMesh> GenerateSphereMesh(const prev_test::common::intersection::Sphere& sphere)
     {
         const float degreesHorizontal{ 360.0f };
         const float degreesVertical{ 180.0f };
@@ -114,7 +171,7 @@ namespace {
             stepsY++;
             curAngleY += addAngleY;
         }
-        return { vertices, indices };
+        return std::make_shared<BoundingVolumeMesh>(vertices, indices);
     }
 } // namespace
 
@@ -126,109 +183,109 @@ BoundingVolumeModelFactory::BoundingVolumeModelFactory(prev::core::memory::Alloc
 std::unique_ptr<prev_test::render::IModel> BoundingVolumeModelFactory::CreateAABBModel(const prev_test::common::intersection::AABB& aabb) const
 {
     const auto aabbPoints{ aabb.GetPoints() };
-    const auto [vertices, indices] = GenerateBoxData(aabbPoints);
+    auto boxMesh = GenerateBoxMesh(aabbPoints);
 
-    const auto verticesDataSize{ sizeof(glm::vec3) * vertices.size() };
+    const auto verticesDataSize{ boxMesh->GetVertexLayout().GetStride() * boxMesh->GerVerticesCount() };
     auto vertexBuffer = prev::render::buffer::BufferBuilder{ m_allocator }
                             .SetMemoryType(prev::core::memory::MemoryType::HOST_MAPPED)
                             .SetUsageFlags(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT)
                             .SetSize(verticesDataSize)
-                            .SetData(vertices.data(), verticesDataSize)
+                            .SetData(boxMesh->GetVertexData(), verticesDataSize)
                             .Build();
 
-    const auto indicesDataSize{ sizeof(uint32_t) * indices.size() };
+    const auto indicesDataSize{ sizeof(uint32_t) * boxMesh->GetIndicesCount() };
     auto indexBuffer = prev::render::buffer::BufferBuilder{ m_allocator }
                            .SetMemoryType(prev::core::memory::MemoryType::HOST_MAPPED)
                            .SetUsageFlags(VK_BUFFER_USAGE_INDEX_BUFFER_BIT)
                            .SetSize(indicesDataSize)
-                           .SetData(indices.data(), indicesDataSize)
+                           .SetData(boxMesh->GetIndices().data(), indicesDataSize)
                            .Build();
 
-    return std::make_unique<prev_test::render::model::Model>(nullptr, std::move(vertexBuffer), std::move(indexBuffer));
+    return std::make_unique<prev_test::render::model::Model>(std::move(boxMesh), std::move(vertexBuffer), std::move(indexBuffer));
 }
 
 std::unique_ptr<prev_test::render::IModel> BoundingVolumeModelFactory::CreateAABBModel(const prev_test::common::intersection::AABB& aabb, const std::shared_ptr<prev_test::render::IModel>& model) const
 {
     const auto aabbPoints{ aabb.GetPoints() };
-    const auto [vertices, indices] = GenerateBoxData(aabbPoints);
+    auto boxMesh = GenerateBoxMesh(aabbPoints);
 
     auto vertexBuffer{ model->GetVertexBuffer() };
     auto indexBuffer{ model->GetIndexBuffer() };
-    vertexBuffer->Write(vertices.data(), sizeof(glm::vec3) * vertices.size());
-    indexBuffer->Write(indices.data(), sizeof(uint32_t) * indices.size());
+    vertexBuffer->Write(boxMesh->GetVertexData(), boxMesh->GetVertexLayout().GetStride() * boxMesh->GerVerticesCount());
+    indexBuffer->Write(boxMesh->GetIndices().data(), sizeof(uint32_t) * boxMesh->GetIndicesCount());
 
-    return std::make_unique<prev_test::render::model::Model>(nullptr, std::move(vertexBuffer), std::move(indexBuffer));
+    return std::make_unique<prev_test::render::model::Model>(std::move(boxMesh), std::move(vertexBuffer), std::move(indexBuffer));
 }
 
 std::unique_ptr<prev_test::render::IModel> BoundingVolumeModelFactory::CreateOBBModel(const prev_test::common::intersection::OBB& obb) const
 {
     const auto obbPoints{ obb.GetPoints() };
-    const auto [vertices, indices] = GenerateBoxData(obbPoints);
+    auto boxMesh = GenerateBoxMesh(obbPoints);
 
-    const auto verticesDataSize{ sizeof(glm::vec3) * vertices.size() };
+    const auto verticesDataSize{ boxMesh->GetVertexLayout().GetStride() * boxMesh->GerVerticesCount() };
     auto vertexBuffer = prev::render::buffer::BufferBuilder{ m_allocator }
                             .SetMemoryType(prev::core::memory::MemoryType::HOST_MAPPED)
                             .SetUsageFlags(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT)
                             .SetSize(verticesDataSize)
-                            .SetData(vertices.data(), verticesDataSize)
+                            .SetData(boxMesh->GetVertexData(), verticesDataSize)
                             .Build();
 
-    const auto indicesDataSize{ sizeof(uint32_t) * indices.size() };
+    const auto indicesDataSize{ sizeof(uint32_t) * boxMesh->GetIndicesCount() };
     auto indexBuffer = prev::render::buffer::BufferBuilder{ m_allocator }
                            .SetMemoryType(prev::core::memory::MemoryType::HOST_MAPPED)
                            .SetUsageFlags(VK_BUFFER_USAGE_INDEX_BUFFER_BIT)
                            .SetSize(indicesDataSize)
-                           .SetData(indices.data(), indicesDataSize)
+                           .SetData(boxMesh->GetIndices().data(), indicesDataSize)
                            .Build();
 
-    return std::make_unique<prev_test::render::model::Model>(nullptr, std::move(vertexBuffer), std::move(indexBuffer));
+    return std::make_unique<prev_test::render::model::Model>(std::move(boxMesh), std::move(vertexBuffer), std::move(indexBuffer));
 }
 
 std::unique_ptr<prev_test::render::IModel> BoundingVolumeModelFactory::CreateOBBModel(const prev_test::common::intersection::OBB& obb, const std::shared_ptr<prev_test::render::IModel>& model) const
 {
     const auto obbPoints{ obb.GetPoints() };
-    const auto [vertices, indices] = GenerateBoxData(obbPoints);
+    auto boxMesh = GenerateBoxMesh(obbPoints);
 
     auto vertexBuffer{ model->GetVertexBuffer() };
     auto indexBuffer{ model->GetIndexBuffer() };
-    vertexBuffer->Write(vertices.data(), sizeof(glm::vec3) * vertices.size());
-    indexBuffer->Write(indices.data(), sizeof(uint32_t) * indices.size());
+    vertexBuffer->Write(boxMesh->GetVertexData(), boxMesh->GetVertexLayout().GetStride() * boxMesh->GerVerticesCount());
+    indexBuffer->Write(boxMesh->GetIndices().data(), sizeof(uint32_t) * boxMesh->GetIndicesCount());
 
-    return std::make_unique<prev_test::render::model::Model>(nullptr, std::move(vertexBuffer), std::move(indexBuffer));
+    return std::make_unique<prev_test::render::model::Model>(std::move(boxMesh), std::move(vertexBuffer), std::move(indexBuffer));
 }
 
 std::unique_ptr<prev_test::render::IModel> BoundingVolumeModelFactory::CreateSphereModel(const prev_test::common::intersection::Sphere& sphere) const
 {
-    const auto [vertices, indices] = GenerateSphereData(sphere);
+    auto sphereMesh = GenerateSphereMesh(sphere);
 
-    const auto verticesDataSize{ sizeof(glm::vec3) * vertices.size() };
+    const auto verticesDataSize{ sphereMesh->GetVertexLayout().GetStride() * sphereMesh->GerVerticesCount() };
     auto vertexBuffer = prev::render::buffer::BufferBuilder{ m_allocator }
                             .SetMemoryType(prev::core::memory::MemoryType::HOST_MAPPED)
                             .SetUsageFlags(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT)
                             .SetSize(verticesDataSize)
-                            .SetData(vertices.data(), verticesDataSize)
+                            .SetData(sphereMesh->GetVertexData(), verticesDataSize)
                             .Build();
 
-    const auto indicesDataSize{ sizeof(uint32_t) * indices.size() };
+    const auto indicesDataSize{ sizeof(uint32_t) * sphereMesh->GetIndices().size() };
     auto indexBuffer = prev::render::buffer::BufferBuilder{ m_allocator }
                            .SetMemoryType(prev::core::memory::MemoryType::HOST_MAPPED)
                            .SetUsageFlags(VK_BUFFER_USAGE_INDEX_BUFFER_BIT)
                            .SetSize(indicesDataSize)
-                           .SetData(indices.data(), indicesDataSize)
+                           .SetData(sphereMesh->GetIndices().data(), indicesDataSize)
                            .Build();
 
-    return std::make_unique<prev_test::render::model::Model>(nullptr, std::move(vertexBuffer), std::move(indexBuffer));
+    return std::make_unique<prev_test::render::model::Model>(std::move(sphereMesh), std::move(vertexBuffer), std::move(indexBuffer));
 }
 
 std::unique_ptr<prev_test::render::IModel> BoundingVolumeModelFactory::CreateSphereModel(const prev_test::common::intersection::Sphere& sphere, const std::shared_ptr<prev_test::render::IModel>& model) const
 {
-    const auto [vertices, indices] = GenerateSphereData(sphere);
+    auto sphereMesh = GenerateSphereMesh(sphere);
 
     auto vertexBuffer{ model->GetVertexBuffer() };
     auto indexBuffer{ model->GetIndexBuffer() };
-    vertexBuffer->Write(vertices.data(), sizeof(glm::vec3) * vertices.size());
-    indexBuffer->Write(indices.data(), sizeof(uint32_t) * indices.size());
+    vertexBuffer->Write(sphereMesh->GetVertexData(), sphereMesh->GetVertexLayout().GetStride() * sphereMesh->GetIndicesCount());
+    indexBuffer->Write(sphereMesh->GetIndices().data(), sizeof(uint32_t) * sphereMesh->GetIndicesCount());
 
-    return std::make_unique<prev_test::render::model::Model>(nullptr, std::move(vertexBuffer), std::move(indexBuffer));
+    return std::make_unique<prev_test::render::model::Model>(std::move(sphereMesh), std::move(vertexBuffer), std::move(indexBuffer));
 }
 } // namespace prev_test::component::ray_casting
