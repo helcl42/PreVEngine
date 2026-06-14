fb312704d2c26742f0766f4cda27b54fe43c0eba
helcll <lubos.helcl@gmail.com>
2026-06-14 23:11:00 +0200

    Remove WebEngineImpl

--- PreVEngine/prev/core/engine/impl/EngineImplFactory.cpp
+++ PreVEngine/prev/core/engine/impl/EngineImplFactory.cpp
@@ -1,9 +1,6 @@
 #include "EngineImplFactory.h"
 
 #include "DefaultEngineImpl.h"
-#if defined(__EMSCRIPTEN__)
-#include "WebEngineImpl.h"
-#endif
 #ifdef ENABLE_XR
 #include "XrEngineImpl.h"
 #endif
@@ -13,8 +10,6 @@ std::unique_ptr<EngineImpl> EngineImplFactory::Create(const Config& config) cons
 {
 #ifdef ENABLE_XR
     return std::make_unique<XrEngineImpl>(config);
-#elif defined(__EMSCRIPTEN__)
-    return std::make_unique<WebEngineImpl>(config);
 #else
     return std::make_unique<DefaultEngineImpl>(config);
 #endif
